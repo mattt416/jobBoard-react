0e7bd3cfc58c1aa0c530bd7abf68acd59f02c436
Matt Thompson <mattt@defunct.ca>
2018-03-02 14:08:56 -0500

    Use simpl-schema for validation

--- imports/ui/Signup.js
+++ imports/ui/Signup.js
@@ -22,6 +22,10 @@ export default class Signup extends React.Component {
     let email = this.refs.email.value.trim();
     let password = this.refs.password.value.trim();
 
+    if (password.length < 9) {
+      return this.setState({error: 'Password must be more than 8 characters long'});
+    }
+
     Accounts.createUser({email, password}, (err) => {
       if (err) {
         this.setState({error: err.reason});
